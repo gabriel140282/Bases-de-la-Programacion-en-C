38b4742f3390ecefb74690d97e5b3afba8b60089
gabriel140282 <gkaakmor@myuax.com>
2024-11-04 23:17:40 +0100

    VFinal

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "Ejercicio1/Perfil.h"
+#include "Ejercicio2/instrucciones-prueba.h"
+#include "Ejercicio3/Bucles.h"
+#include "Ejercicio4/Arrays.h"
+#include "Ejercicio5/Funciones.h"
+#include "Ejercicio6/punteros.h"
+#include "Ejercicio7/funcion.h"
+#include "Ejercicio8/constantes.h"
+
+int main() {
+    perfil();
+    instrucciones_prueba();
+    operadores();
+    bucles();
+    arrays();
+    funcion_suma(int a, int b);
+    retorno(int a);
+    palabra();
+    muestra(int a);
+    muestra(string a);
+    punteros_variables();
+    punteros_arrays();
+    asignacion_memoria();
+    aritmetica_punteros();
+    punteros_punteros();
+    calculo();
+    constantes_simbolicas();
+    tipo_void();
+    return 0;
+}
